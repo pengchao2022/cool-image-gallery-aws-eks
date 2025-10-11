995010c46db9b46e235901999419067904feff67
pengchao <pengchao.ma2@outlook.com>
2025-10-11 17:25:28 +0800

    modified again

---
+++ terraform/backend.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "terraformstatefile090909"
+    key            = "image-for-eks.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "comic-website-tfstate-lock"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}                                           
