2de7fd64436dc0069c0d0d116755d632d155e995
pengchao <pengchao.ma2@outlook.com>
2025-10-12 12:46:00 +0800

    modified

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -46,7 +46,7 @@ resource "aws_db_instance" "main" {
 
   # Database Configuration
   engine                = "postgres"
-  engine_version        = "14.12"
+  engine_version        = "14.17"
   instance_class        = var.instance_class
   allocated_storage     = var.allocated_storage
   max_allocated_storage = var.max_allocated_storage
