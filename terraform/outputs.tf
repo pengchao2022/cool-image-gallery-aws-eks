d233f345671158c13e49443fd4a85465c82e35a1
pengchao <pengchao.ma2@outlook.com>
2025-10-18 13:05:26 +0800

    modified the node number again

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -145,11 +145,12 @@ output "redis_port" {
 }
 
 # EC2 Outputs
+/*
 output "ec2_public_ip" {
   description = "EC2 instance public IP address"
   value       = module.ec2.ec2_public_ip
 }
-/*
+
 output "ec2_public_dns" {
   description = "EC2 instance public DNS name"
   value       = module.ec2.ec2_public_dns
