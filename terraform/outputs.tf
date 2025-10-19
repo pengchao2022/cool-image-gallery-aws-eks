ca9c915f39b5bebe6ba5e1b48651ded81e4a9c45
pengchao <pengchao.ma2@outlook.com>
2025-10-19 13:34:32 +0800

    modified again

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -145,7 +145,7 @@ output "redis_port" {
 }
 
 # EC2 Outputs
-
+/*
 output "ec2_public_ip" {
   description = "EC2 instance public IP address"
   value       = module.ec2.ec2_public_ip
@@ -160,7 +160,7 @@ output "ssh_connection_command" {
   description = "SSH connection command"
   value       = "ssh -i ~/.ssh/id_rsa ubuntu@${module.ec2.ec2_public_ip}"
 }
-
+*/
 
 output "community_repository_url" {
   description = "Community ECR repository URL"
